5e5454c17062b57215c60a21868de6311f8ab15d
Krzysztof Antoniak <krzysztof@antoniak.in>
2017-03-25 17:05:09 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Description Plugin
+
+A simple plugin to show custom content before article main text.
+
+## License
+
+Copyright © 2017 Krzysztof Antoniak (http://antoniak.in/). Licensed under GNU General Public License, version 3.0 (GPL-3.0).
---
+++ kantoniak-project-description.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Project Description
+Plugin URI: http://antoniak.in/
+Description: This plugin adds project description at the top of the article.
+Version: 0.0.0
+Author: Krzysztof Antoniak
+Author URI: http://antoniak.in/
+License: GNU General Public License, version 3.0 (GPL-3.0)
+ */
