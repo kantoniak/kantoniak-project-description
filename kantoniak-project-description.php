88a2e08b7cc75e884f951db806fc258acfdfc247
Krzysztof Antoniak <krzysztof@antoniak.in>
2017-03-25 17:45:59 +0100

    Add filter to render box before every article

--- kantoniak-project-description.php
+++ kantoniak-project-description.php
@@ -8,3 +8,30 @@ Author: Krzysztof Antoniak
 Author URI: http://antoniak.in/
 License: GNU General Public License, version 3.0 (GPL-3.0)
  */
+
+namespace kantoniak {
+
+class ProjectDescription {
+
+
+  public function __construct() {
+    add_filter('the_content', array($this, 'prependWithProjectDescription'));
+  }
+
+  public function prependWithProjectDescription($content) {
+    if (!is_single() || !in_the_loop() || !is_main_query() ) {
+        return $content;
+    }
+ 
+    return $this->renderBox().$content;
+  }
+
+  protected function renderBox() {
+    ob_start();
+    include('template-box.php');
+    return ob_get_clean();
+  }
+}
+
+$projectDesc = new ProjectDescription();
+}
---
+++ template-box.php
@@ -0,0 +1,8 @@
+<aside id="project-description">
+  <h2>Project name</h2>
+  <a href="#project-description-end">Jump to content</a>
+  <div class="content">
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+</aside>
+<a name="project-description-end" />
